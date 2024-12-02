cc03eaf946950ac0ff9717cf9ae3c6a435f24256
Laswer5 <lukas.lindenthoming.9997@student.uu.se>
2024-12-02 10:16:05 +0100

    Add login callbacks - need to add DB connection

--- source/battle-board/battle-board/auth.ts
+++ source/battle-board/battle-board/auth.ts
@@ -2,5 +2,22 @@ import NextAuth from "next-auth"
 import Discord from "next-auth/providers/discord"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Discord]
-})
+  providers: [Discord({
+    profile(profile) {
+      console.log("Profile: " + JSON.stringify(profile));
+      return profile;
+    }
+  })],
+  callbacks: {
+    jwt({ token, user }) {
+      console.log("User: " + JSON.stringify(user))
+      console.log("Token: " + JSON.stringify(token))
+      return token
+    },
+    session({ session, token }) {
+      console.log("Session: " + JSON.stringify(session))
+      return session
+    }
+  }
+}
+);
