839ad8f371c0298fc52dd4d2c6b158dccc63faf5
Nagnub <morgan999@live.se>
2024-12-04 00:13:32 +0100

    hover effect on toggle button

--- source/battle-board/battle-board/components/search/search-toggle-btn.tsx
+++ source/battle-board/battle-board/components/search/search-toggle-btn.tsx
@@ -2,7 +2,7 @@ export default function SearchToggleButton() {
 
     return (
         <div className="flex items-center justify-end appearance-none bg-white shadow-sm shadow-white rounded-full h-8 w-20 p-1">
-        <div className="h-7 w-7 rounded-full bg-buttonprimary "></div>
+        <div className="h-7 w-7 rounded-full bg-buttonprimary hover:bg-buttonprimaryhover"></div>
       </div>
     );
 }
