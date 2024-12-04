24d26c67cf147d392100dc9bff005a639baae092
Gustavzon <Anton.gustavsson1999@outlook.com>
2024-12-04 14:08:00 +0100

    något

--- source/battle-board/battle-board/app/user/[username]/page.tsx
+++ source/battle-board/battle-board/app/user/[username]/page.tsx
@@ -1,11 +1,28 @@
-//Interface for specific user based on the user id and stuff from discord
-//This will be the main page for the user to see their own profile
+"use client";
 
-export default function Profile() {
-  return (
-    <main className=" flex-auto font-odibee text-9xl text-center">
-      {" "}
-      The Profile Placeholder Page
-    </main>
-  );
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+
+export default function LoginBtn() {
+  const { data: session } = useSession();
+
+  console.log("Session Data:", session?.user.name); // Log the session data to debug
+
+  if (session?.user) {
+
+    return (
+
+      <main>
+        <p className="text-center">Logged in as {session.user.name}</p>
+      </main>
+
+    )
+  } else {
+
+    return (
+      <div>
+        <p className="text-center">Not logged in</p>
+      </div>
+    )
+  }
 }
