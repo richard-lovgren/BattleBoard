15bcd93ca41af8e802d004ad7200732cdf4ace51
Richard Lövgren <73587008+richard-lovgren@users.noreply.github.com>
2024-12-04 23:12:20 +0100

    Fixed apostrophy for Vercel

--- source/battle-board/battle-board/app/user/[username]/page.tsx
+++ source/battle-board/battle-board/app/user/[username]/page.tsx
@@ -29,7 +29,7 @@ export default function LoginBtn() {
             <h2 className="font-bold">{session.user.display_name}</h2>
             <h2 className="font-bold">
               I like to play{" "}
-              <span className="text-foreground">League Of Legends.</span> It's
+              <span className="text-foreground">League Of Legends.</span> It&apos;s
               actually my most favourite of games. I get great enjoyment and
               relaxation out of playing league. The rift is calling....
             </h2>
