d5615a1d2149dffb9cf977d5bcfdfd1cb17ee266
natta p <natashazaza@live.se>
2024-12-09 12:04:22 +0100

    done with visuals for create community and create competition

--- source/battle-board/battle-board/app/createCompetition/page.tsx
+++ source/battle-board/battle-board/app/createCompetition/page.tsx
@@ -41,12 +41,41 @@ export default function CreateCompetition() {
 
           <div className='createGroup'>
             <div className='text-5xl'>Settings</div>
-            
+              
+  
+            <button className='uploadButton font-nunito'>Public</button>
+            <button className='uploadButton font-nunito'>Private</button>
+
 
           </div>
 
           <div className='createGroup'>
-            <div className='text-5xl'>Invite community members</div>
+            <div className='text-5xl'>Choose game</div>
+            <div className='search-bar flex items-center rounded-full border-solid border-[5px] h-[50px] w-[50vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50'>
+              <input className=' text-3xl text-left w-full'></input>
+            </div>
+          </div>
+          <div className='createGroup'>
+            <div className='text-5xl'>Choose mode</div>
+            
+            <div className="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8">
+            <div className="border border-white rounded-lg p-6 w-64">
+              <h3 className="text-xl font-bold">Tournament</h3>
+              <img src="/tournament.svg" alt="Tournament Mode" className="mt-4" />
+            </div>
+            <div className="border border-white rounded-lg p-6 w-64">
+              <h3 className="text-xl font-bold">Classic</h3>
+              <img src="/classic_mode_icon.svg" alt="Classic Mode" className="mt-4" />
+            </div>
+            <div className="border border-white rounded-lg p-6 w-64">
+              <h3 className="text-xl font-bold">Rival</h3>
+              <img src="/rival_mode_icon.svg" alt="Rival Mode" className="mt-4" />
+            </div>
+          </div>
+
+          </div>
+          <div className='createGroup'>
+            <div className='text-5xl'>Invite players</div>
             <div className='search-bar flex items-center rounded-full border-solid border-[5px] h-[50px] w-[50vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50'>
               <input className=' text-3xl text-left w-full'></input>
             </div>
