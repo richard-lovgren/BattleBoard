35ff0c421698924db8d9390d6acd800f14ccfe52
Richard Lövgren <73587008+richard-lovgren@users.noreply.github.com>
2024-12-12 11:22:40 +0100

    Update next.config.ts

--- source/battle-board/battle-board/next.config.ts
+++ source/battle-board/battle-board/next.config.ts
@@ -4,7 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "cdn.discordapp.com" }],
   },
-  /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
