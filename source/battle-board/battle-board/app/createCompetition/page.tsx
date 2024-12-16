cee3246ec74715516fab01ffe08481cf16912ca5
Morgan Räsänen <61024214+Nagnub@users.noreply.github.com>
2024-12-16 10:30:07 +0100

    you can now post a new competition to the db, requires that game exsists (#57)
    * you can now post a new competition to the db, requires that game exsists
    
    * Changed to useEffect so client side works
    
    ---------
    
    Co-authored-by: Richard Lövgren <73587008+richard-lovgren@users.noreply.github.com>

--- source/battle-board/battle-board/app/createCompetition/page.tsx
+++ source/battle-board/battle-board/app/createCompetition/page.tsx
@@ -1,163 +1,192 @@
-"use client"
+'use client';
 
-//import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CompetitionDto from '@/models/dtos/competition-dto';
-import './createCompetition.css'
+import './createCompetition.css';
+import Game from '@/models/game';
 
 export default function CreateCompetition() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const placeholderUsers: string[] = ["laswer5", ".nattap", "cafg", "richard", "nagnub"];
 
-  // const competition : CompetitionDto = {
-  //   competition_name: "Test use state",
-  //   competition_description: "test desc",
-  //   competition_type: 1,
-  //   format: 1,
-  //   is_open: true,
-  //   is_running: true,
-  //   game_id: "8ffdbc35-84ab-4e3b-8743-789a59939e59",
-  //   rank_alg: 1,
-  //   is_public: true,
-  // };
+  // Fetch game data
+  useEffect(() => {
+    const fetchGameData = async () => {
+      try {
+        const response = await fetch(`/api/games`);
+        if (!response.ok) {
+          throw new Error(`Error fetching games: ${response.statusText}`);
+        }
+        const result: Game[] = await response.json();
+        setGames(result);
+      } catch (error) {
+        console.error("Error in fetchGameData:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  //const [newCompetition, setNewCompetition] = useState<CompetitionDto | null>(null);
+    fetchGameData();
+  }, []);
 
-  // const updateNewCompetitionState = async () => {
-  //   setNewCompetition(competition);
-  //   console.log(newCompetition);
-  // }
-
-  const handleTitleChange = async () => {
-
-  }
-
-  const postCompetitionData = async () => {
+  async function postCompetitionData(body: CompetitionDto) {
     try {
-      const body: CompetitionDto = {
-        competition_name: "Test 2",
-        competition_description: "test desc",
-        competition_type: 1,
-        format: 1,
-        is_open: true,
-        is_running: true,
-        game_id: "8ffdbc35-84ab-4e3b-8743-789a59939e59",
-        rank_alg: 1,
-        is_public: true,
-      };
-
-
       console.log("Creating competition with body::", body);
       const url = `/api/competitions`;
 
-      const response = await fetch(url, 
-        {
-          method: "POST",
-          body: JSON.stringify(body),
-        }
-      )
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
       if (!response.ok) {
         throw new Error(`Error creating competition: ${response.statusText}`);
       }
 
       const result = await response.json();
       console.log(result.message);
-
-
     } catch (error) {
-        console.error("Error in postCompetitionData:", error);
+      console.error("Error in postCompetitionData:", error);
     }
-  };
+  }
+
+  async function handleSumbit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    const formJson = Object.fromEntries(formData.entries());
+    const body: CompetitionDto = {
+      competition_name: formJson.competitionName.toString(),
+      competition_description: formJson.competitionDesc.toString(),
+      competition_type: parseInt(formJson.competitionType.toString()),
+      format: 1,
+      is_open: true,
+      is_running: true,
+      game_id: formJson.game.toString(),
+      rank_alg: 1,
+      is_public: !!formJson.isPublic,
+    };
+
+    console.log(body);
+    await postCompetitionData(body);
+  }
 
   return (
     <div className='bg-background flex flex-col items-center'>
       <main className='flex-auto item font-odibee text-9xl'>
-        <div className='createWrapper'>
+        <form method='post' onSubmit={handleSumbit} className='createWrapper'>
           <div className='text-6xl'>
             Create a competition
             <hr />
           </div>
 
+          {/* Title and Date */}
           <div className='createGroupTwoCols'>
             <div className='createGroup'>
-              <div className='text-5xl '>Title</div>
+              <label className='text-5xl'>Title</label>
               <div className='search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[30vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50'>
-                <input onChange={handleTitleChange} className=' text-3xl text-left w-full'></input>
+                <input name='competitionName' className='text-3xl text-left w-full' />
               </div>
             </div>
             <div className='createGroup'>
-              <div className='text-5xl'>Date</div>
+              <label className='text-5xl'>Date</label>
               <div className='search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[30vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50'>
-                <input className=' text-3xl text-left w-full'></input>
+                <input name='date' className='text-3xl text-left w-full' />
               </div>
             </div>
           </div>
 
+          {/* Description */}
           <div className='createGroup'>
-              <div className='text-5xl '>Description</div>
-              <div className='search-bar flex items-center rounded-3xl border-solid border-white border-[5px] h-[150px] w-[62vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50'>
-                <input className=' text-3xl text-left w-full'></input>
-              </div>
-            </div>
-
-          <div className='createGroup'>
-            <div className='text-5xl'>Add a cover image</div>
-            <button className='uploadButton font-nunito'>Upload image</button>
+            <label className='text-5xl'>
+              Description
+              <textarea
+                name='competitionDesc'
+                className='appearance-none focus:outline-none mt-5 p-4 flex items-center text-3xl rounded-3xl bg-[#0E0030] border-solid border-white border-[5px] h-[150px] w-[62vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50'
+              ></textarea>
+            </label>
           </div>
 
+          {/* Cover Image */}
+          <div className='createGroup'>
+            <label className='text-5xl'>Add a cover image</label>
+            <button type='button' className='uploadButton font-nunito'>Upload image</button>
+          </div>
+
+          {/* Settings */}
           <div className='createGroup'>
             <div className='text-5xl'>Settings</div>
-              
-  
-            <button className='uploadButton font-nunito'>Public</button>
-            <button className='uploadButton font-nunito'>Private</button>
-
-
+            <label className='font-nunito textshadow text-xl hover:cursor-pointer w-fit'>
+              <input className='appearance-auto' type='radio' name='isPublic' value={1} defaultChecked /> Public
+            </label>
+            <label className='font-nunito textshadow text-xl hover:cursor-pointer w-fit'>
+              <input className='appearance-auto' type='radio' name='isPublic' value={0} /> Private
+            </label>
           </div>
 
+          {/* Games */}
           <div className='createGroup'>
-            <div className='text-5xl'>Choose game</div>
+            <label className='text-5xl'>Choose game</label>
             <div className='search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[50vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50'>
-              <input className=' text-3xl text-left w-full'></input>
+              {loading ? (
+                <div>Loading games...</div>
+              ) : (
+                <select
+                  className='decorated text-3xl text-left w-full bg-[#0E0030] rounded-full focus:outline-none'
+                  name='game'
+                >
+                  {games.map((game) => (
+                    <option key={game.id} value={game.id}>
+                      {game.game_name}
+                    </option>
+                  ))}
+                </select>
+              )}
             </div>
           </div>
+
+          {/* Modes */}
           <div className='createGroup'>
             <div className='text-5xl'>Choose mode</div>
-            
-          <div className="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8">
-            <div className="border-solid border-white border-[5px] shadow-lg shadow-indigo-500/50 rounded-3xl  p-6 w-64">
-              <h3 className="textshadow text-xl font-bold">Tournament</h3>
-              <img
-                src="/tournament.svg"
-                alt="Tournament Mode"
-                className="mt-4"
-              />
-            </div>
-            <div className="border-solid border-white border-[5px] shadow-lg shadow-indigo-500/50 rounded-3xl  p-6 w-64">
-              <h3 className="textshadow text-xl font-bold">Classic</h3>
-              <img
-                src="/classic_mode_icon.svg"
-                alt="Classic Mode"
-                className="mt-4"
-              />
-            </div>
-            <div className="border-solid border-white border-[5px] shadow-lg shadow-indigo-500/50 rounded-3xl  p-6 w-64">
-              <h3 className="textshadow text-xl font-bold">Rival</h3>
-              <img
-                src="/rival_mode_icon.svg"
-                alt="Rival Mode"
-                className="mt-4"
-              />
-            </div>
+            <label className='font-nunito textshadow text-xl hover:cursor-pointer w-fit'>
+              <input className='appearance-auto' type='radio' name='competitionType' value={1} defaultChecked /> Tournament
+            </label>
+            <label className='font-nunito textshadow text-xl hover:cursor-pointer w-fit'>
+              <input className='appearance-auto' type='radio' name='competitionType' value={2} /> Classic
+            </label>
+            <label className='font-nunito textshadow text-xl hover:cursor-pointer w-fit'>
+              <input className='appearance-auto' type='radio' name='competitionType' value={3} /> Rival
+            </label>
           </div>
-          </div>
+
+          {/* Players */}
           <div className='createGroup'>
-            <div className='text-5xl'>Invite players</div>
+            <label className='text-5xl'>Invite players</label>
             <div className='search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[50vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50'>
-              <input className=' text-3xl text-left w-full'></input>
+              <select
+                className='decorated text-3xl text-left w-full bg-[#0E0030] rounded-full focus:outline-none'
+                name='invitedUsers'
+              >
+                {placeholderUsers.map((user) => (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                ))}
+              </select>
             </div>
           </div>
 
-          <button onClick={postCompetitionData} className='createButton font-nunito'>Save</button>
-        </div>
+          <button type='submit' className='createButton font-nunito'>
+            Save
+          </button>
+        </form>
       </main>
     </div>
-  )
+  );
 }
