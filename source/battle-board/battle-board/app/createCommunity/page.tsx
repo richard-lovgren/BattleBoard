11cdbba90e18aa2271584056088df14f71bacba3
Laswer5 <lukas.lindenthoming.9997@student.uu.se>
2024-12-16 01:14:57 +0100

    Fixed build issues
    Added spinning loading indicator when waiting for community ID for redirect

--- source/battle-board/battle-board/app/createCommunity/page.tsx
+++ source/battle-board/battle-board/app/createCommunity/page.tsx
@@ -5,7 +5,7 @@ import './createCommunity.css';
 export default function CreateCommunity() {
 
   const redirectToDiscord = () => {
-    const user_profile_url = window.location.href.replace("createCommunity", "createCommunity/fetchCreatedCommunity");
+    const user_profile_url = window.location.href.replace("createCommunity", "fetchCreatedCommunity");
     const bot_url = "https://discord.com/oauth2/authorize?client_id=1308029231342555136&permissions=18136036801537&integration_type=0&scope=bot" + "&redirect_uri=" + encodeURIComponent(user_profile_url) + "&response_type=code";
     window.location.href = bot_url;
   };
@@ -16,13 +16,7 @@ export default function CreateCommunity() {
         <div className='createWrapper'>
 
           <div className='createGroup'>
-            <div className='text-3xl'>Add a cover image</div>
-            <button className='uploadButton font-nunito'>
-              Upload image
-            </button>
-          </div>
-
-          <div className='createGroup'>
+          <div className='text-3xl'>Create a community</div>
             <button 
               className='uploadButton font-nunito' 
               onClick={() => redirectToDiscord()}
