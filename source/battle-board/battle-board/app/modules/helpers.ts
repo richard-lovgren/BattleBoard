f585ed14e124c94b327416d347669f0e7f7b7fe8
Morgan Räsänen <61024214+Nagnub@users.noreply.github.com>
2024-12-18 12:05:24 +0100

    skibidi

--- source/battle-board/battle-board/app/modules/helpers.ts
+++ source/battle-board/battle-board/app/modules/helpers.ts
@@ -1,9 +1,9 @@
 export default function formatDate(dateStr: Date): string {
   const date = new Date(dateStr);
-  const formattedDate = date.toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  const formattedDate = date.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
 
   return formattedDate;
