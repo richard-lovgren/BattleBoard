7972c4adc378c8cb267939a12ada28e9fa2eceb6
Richard Lövgren <73587008+richard-lovgren@users.noreply.github.com>
2024-12-20 10:07:22 +0100

    Fixed build errors

--- source/battle-board/battle-board/models/dtos/leaderboard-dto.ts
+++ source/battle-board/battle-board/models/dtos/leaderboard-dto.ts
@@ -1,6 +1,6 @@
 
 export default interface LeaderboardDTO {
     "competition_id": string;
-    "column_names": string[];
-    "leaderboard_entries": Array<Record<string, string>>,
+    "column_names"?: string[];
+    "leaderboard_entries"?: Array<Record<string, string>>,
 }
