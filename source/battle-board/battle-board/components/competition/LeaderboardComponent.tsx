9d4a0996541a3114e8cb24ad432cc2de53f51cef
Laswer5 <lukas.lindenthoming.9997@student.uu.se>
2024-12-21 02:36:48 +0100

    Reload leaderboard component on file parse (get new data)
    Fixed bugs in parsing logic

--- source/battle-board/battle-board/components/competition/LeaderboardComponent.tsx
+++ source/battle-board/battle-board/components/competition/LeaderboardComponent.tsx
@@ -1,32 +1,33 @@
 "use client";
 
-import { Suspense, useState, useEffect } from "react";
+import { Suspense, useState } from "react";
 import EditCompetitionButton from "@/components/competition/EditCompetitionButton";
 import FileUploadAndParseComponent from "@/components/competition/FileUploadAndParseButton";
 import { Leaderboard } from "@/models/leaderboard";
+import CompetitionData from "@/models/interfaces/CompetitionData";
+import CompetitonModeWrapper from '@/components/competition/CompetitionModeWrapper';
 
 const LeaderboardComponent = ({
   competitionId,
+  competitionData,
   creatorName,
   initialLeaderboard,
   userNames,
 }: {
   competitionId: string;
+  competitionData: CompetitionData;
   creatorName: string;
   initialLeaderboard: Leaderboard | null;
   userNames: string[] | null;
 }) => {
-  const [leaderboardData, setLeaderboardData] = useState<Leaderboard | null>(
-    null
-  );
+  
 
-  useEffect(() => {
-    setLeaderboardData(initialLeaderboard);
-  }, [initialLeaderboard]);
+  const [reload, setReload] = useState(0);
 
-  const handleCompetitionDataParsed = (data: Leaderboard) => {
-    setLeaderboardData(data);
-  };
+  const triggerReload = () => {
+    console.log("triggering reload");
+    setReload((prev) => prev + 1);
+  }
 
   if (userNames === null) {
     return <p>silli :P</p>;
@@ -37,11 +38,15 @@ const LeaderboardComponent = ({
       epic leaderboard
       <EditCompetitionButton competitionCreator={creatorName} />
       <FileUploadAndParseComponent
-        prevLeaderboard={leaderboardData}
+        prevLeaderboard={initialLeaderboard}
         userNames={userNames}
-        handleCompetitionDataParsed={handleCompetitionDataParsed}
+        handleCompetitionDataParsed={triggerReload}
         competitionId={competitionId}
       />
+
+      <CompetitonModeWrapper mode={competitionData.competition_type} competitionId={competitionId} reloadTrigger={reload} />
+
+
     </Suspense>
   );
 };
