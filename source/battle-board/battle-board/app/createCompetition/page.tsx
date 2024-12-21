5bc3ba4047a8c9b9e98a9f4978514d690a59961e
Nagnub <morgan999@live.se>
2024-12-21 23:35:56 +0100

    WIP functionality improvement for create competition, uses some more MUI components

--- source/battle-board/battle-board/app/createCompetition/page.tsx
+++ source/battle-board/battle-board/app/createCompetition/page.tsx
@@ -13,39 +13,94 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import dayjs, { Dayjs } from "dayjs";
 import GeneralButton from "@/components/general-btn";
 
-export default function CreateCompetition() {
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
+import User from "@/models/user";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const placeholderUsers: string[] = [
+  "laswer5",
+  ".nattap",
+  "cafg",
+  "richard",
+  "nagnub",
+];
+
+export default function CreateCompetitionPage() {
   const [games, setGames] = useState<Game[]>([]);
+  const [game, setGame] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
+  const [participants, setParticipants] = useState<string[]>([]);
   const [loading, setLoading] = useState(true);
   const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
-  const placeholderUsers: string[] = [
-    "laswer5",
-    ".nattap",
-    "cafg",
-    "richard",
-    "nagnub",
-  ];
   const session = useSession();
   const router = useRouter();
   const username = session.data?.user.name || "undefined";
+  
+
+  const handleParticipantsChange = (event: SelectChangeEvent<typeof participants>) => {
+    const {
+      target: { value },
+    } = event;
+    setParticipants(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  const handleGameChange = (event: SelectChangeEvent) => {
+    setGame(event.target.value as string);
+  };
+
+  const fetchGameData = async () => {
+    try {
+      const response = await fetch(`/api/games`);
+      if (!response.ok) {
+        throw new Error(`Error fetching games: ${response.statusText}`);
+      }
+      const result: Game[] = await response.json();
+      setGames(result);
+    } catch (error) {
+      console.error("Error in fetchGameData:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch(`/api/users`);
+      if (!response.ok) {
+        throw new Error(`Error fetching users: ${response.statusText}`);
+      }
+      const result: User[] = await response.json();
+      setUsers(result);
+    } catch (error) {
+      console.error("Error in fetchUserData:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   // Fetch game data
   useEffect(() => {
-    const fetchGameData = async () => {
-      try {
-        const response = await fetch(`/api/games`);
-        if (!response.ok) {
-          throw new Error(`Error fetching games: ${response.statusText}`);
-        }
-        const result: Game[] = await response.json();
-        setGames(result);
-      } catch (error) {
-        console.error("Error in fetchGameData:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchGameData();
+    fetchUserData();
   }, []);
 
   if (!session.data) {
@@ -104,7 +159,7 @@ export default function CreateCompetition() {
       format: 1,
       is_open: true,
       is_running: true,
-      game_id: formJson.game.toString(),
+      game_id: game,
       rank_alg: 1,
       is_public: !!formJson.isPublic,
       //competition_date: selectedDate?.toISOString() || null, // TODO: add to db
@@ -141,20 +196,18 @@ export default function CreateCompetition() {
               <div className="search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[30vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50">
                 <input
                   name="competitionName"
-                  className="text-3xl text-left w-full"
+                  className=" font-nunito textshadow appearance-none text-3xl text-left w-full"
                 />
               </div>
             </div>
-            <div className="createGroup">
+            <div className="ml-32 createGroup">
               <label className="text-5xl">Start date</label>
-              <div className="search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[30vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50">
+              <div className="search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[20vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50">
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                   <DatePicker
-                    label="Select Date"
+                    label="Select date"
                     onChange={(newValue) => setSelectedDate(newValue)}
-                    slotProps={{
-                      textField: { variant: "outlined", fullWidth: true },
-                    }} // Improve appearance
+                    sx={{ width: '20vw'}}
                   />
                 </LocalizationProvider>
               </div>
@@ -167,7 +220,7 @@ export default function CreateCompetition() {
               Description
               <textarea
                 name="competitionDesc"
-                className="appearance-none focus:outline-none mt-5 p-4 flex items-center text-3xl rounded-3xl bg-[#0E0030] border-solid border-white border-[5px] h-[150px] w-[62vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50"
+                className="appearance-none font-nunito textshadow focus:outline-none mt-5 p-4 flex items-center text-3xl rounded-3xl bg-[#0E0030] border-solid border-white border-[5px] h-[150px] w-[62vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50"
               ></textarea>
             </label>
           </div>
@@ -207,8 +260,8 @@ export default function CreateCompetition() {
           {/* Games */}
           <div className="createGroup">
             <label className="text-5xl">Choose game</label>
-            <div className="search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[50vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50">
-              {loading ? (
+            <div className="search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[28vw] py-10 pl-4 pr-8 shadow-lg shadow-indigo-500/50">
+              {/* {loading ? (
                 <div>Loading games...</div>
               ) : (
                 <select
@@ -221,7 +274,26 @@ export default function CreateCompetition() {
                     </option>
                   ))}
                 </select>
+              )} */}
+              {loading ? (
+                <div className=" font-nunito textshadow">Loading games...</div>
+              ) : (
+                <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Select game</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={game}
+                  label="Select game"
+                  onChange={handleGameChange}
+                >
+                  {games.map((game) => (
+                    <MenuItem key={game.id} value={game.id}>{game.game_name}</MenuItem>
+                ))}
+                </Select>
+              </FormControl>
               )}
+
             </div>
           </div>
 
@@ -264,8 +336,8 @@ export default function CreateCompetition() {
           {/* Players */}
           <div className="createGroup">
             <label className="text-5xl">Invite players</label>
-            <div className="search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[50vw] py-8 pl-4 pr-8 shadow-lg shadow-indigo-500/50">
-              <select
+            <div className="search-bar flex items-center rounded-full border-solid border-white border-[5px] h-[50px] w-[28vw] py-10 pl-4 pr-8 shadow-lg shadow-indigo-500/50">
+              {/* <select
                 className="decorated text-3xl text-left w-full bg-[#0E0030] rounded-full focus:outline-none"
                 name="invitedUsers"
               >
@@ -274,9 +346,32 @@ export default function CreateCompetition() {
                     {user}
                   </option>
                 ))}
-              </select>
+              </select> */}
+            <FormControl
+              sx={{ m: 1, width: '28vw' }}
+            >
+              <InputLabel id="multiple-checkbox-label">Select players</InputLabel>
+              <Select
+                labelId="multiple-checkbox-label"
+                id="multiple-checkbox"
+                multiple={true}
+                value={participants}
+                onChange={handleParticipantsChange}
+                input={<OutlinedInput label="Select layers" />}
+                renderValue={(selected) => (selected as string[]).join(', ')}
+                MenuProps={MenuProps}
+              >
+                {users.map((user) => (
+                  <MenuItem key={user.id} value={user.user_name}>
+                    <Checkbox checked={participants.includes(user.user_name)} />
+                    <ListItemText primary={user.user_name} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
             </div>
           </div>
+
           <GeneralButton text="Create competition" type="submit" />
         </form>
       </main>
---
+++ source/battle-board/battle-board/models/user.ts
@@ -0,0 +1,7 @@
+export default interface User {
+    id: string;
+    discord_id: bigint,
+    user_name: string,
+    display_name: string,
+    league_puuid: string;
+}
