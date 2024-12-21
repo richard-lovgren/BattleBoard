6b3d546f30e4547b8fad74c073f5788174792dfe
natta p <natashazaza@live.se>
2024-12-21 02:03:36 +0100

    style on competition page

--- source/battle-board/battle-board/app/competition/[competition]/page.tsx
+++ source/battle-board/battle-board/app/competition/[competition]/page.tsx
@@ -29,7 +29,7 @@ const CompetitionPage = async (props: { params: CompetitionPageProps }) => {
   return (
     <div className="w-full h-full flex flex-col gap-4 items-center">
       <div className="w-full flex text-slate-50 text-3xl flex-row items-center gap-4 px-10 py-0">
-        <div className="relative inline-block flex-grow flex-shrink-0 w-96 h-96">
+        <div className="relative inline-block flex-shrink-0 w-96 h-96">
           <Image
             src="/competition-placeholder.jpg"
             alt="example"
@@ -40,7 +40,7 @@ const CompetitionPage = async (props: { params: CompetitionPageProps }) => {
           <div className="absolute inset-0 bg-black bg-opacity-20 rounded-full shadow-inner shadow-black"></div>
         </div>
 
-        <div className="flex flex-col gap-4 h-full pl-12 pr-36">
+        <div className="flex flex-col gap-4 h-full pl-12 pr-36 flex-grow">
           <h1 className="flex text-8xl font-odibee text-white">
             {competitionData.competition_name}
           </h1>
@@ -54,14 +54,14 @@ const CompetitionPage = async (props: { params: CompetitionPageProps }) => {
           <Image
             src="/rival_mode_icon.svg"
             alt="Competition Type Image"
-            width={100}
-            height={100}
+            width={50}
+            height={50}
           />
           <Image
             src="/user.svg"
             alt="Players Icon Image"
-            width={100}
-            height={100}
+            width={50}
+            height={50}
           />
           <div className="flex flex-col gap-0 items-center">
             <h1 className="flex text-xl font-odibee text-white py-0 my-0">
