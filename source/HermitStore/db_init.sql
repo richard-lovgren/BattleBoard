d122ae3395712056a96183a90331341057879ad2
Morgan Räsänen <61024214+Nagnub@users.noreply.github.com>
2025-01-08 15:30:43 +0100

    edit id so it doesn't mach my test server

--- source/HermitStore/db_init.sql
+++ source/HermitStore/db_init.sql
@@ -150,7 +150,7 @@ INSERT INTO game(id, game_name) VALUES('241e3728-5b50-45a1-92cf-c8695af932e2', '
 
 
 --Communities
-INSERT INTO community(id, community_name, community_members) VALUES(1318571109418926131, 'BattleBoard Team', 5);
+INSERT INTO community(id, community_name, community_members) VALUES(1238571109418926131, 'BattleBoard Team', 5);
 INSERT INTO community(id, community_name, community_members) VALUES(1318571109418926132, 'Pathetic Failure Squad', 10);
 INSERT INTO community(id, community_name, community_members) VALUES(1318571109418926133, 'Uppsala FGC', 50);
 INSERT INTO community(id, community_name, community_members) VALUES(1318571109418966134, 'Gnarp FGC', 8);
