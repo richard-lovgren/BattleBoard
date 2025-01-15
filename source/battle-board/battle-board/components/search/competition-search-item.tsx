5a960402a5456fb4f798df939ace802201d83f68
richard-lovgren <richard.lovgren.3075@student.uu.se>
2025-01-15 14:23:30 +0100

    Revert "Fixed db url for competition image"
    This reverts commit cf41f7b2a6214299eec474d636f95fc32fd56953.

--- source/battle-board/battle-board/components/search/competition-search-item.tsx
+++ source/battle-board/battle-board/components/search/competition-search-item.tsx
@@ -35,43 +35,41 @@ export default function CompetitionSearchItem(competition: CompetitionData) {
     loadGameName()
   }, [competition.game_id])
 
-  const dbUrl = process.env.DB_CONN_STR
-
   return (
     <Link href={`/competition/${competition.id}`} passHref>
-      <div style={{ cursor: 'pointer' }} className="flex flex-none flex-col h-[400px] w-[300px] rounded-[1.2rem] bg-gradient-to-br from-[#4E35BE] to-[#241958] transform transition-transform duration-300 hover:scale-105">
-        <div
-          className='flex flex-none items-center justify-center rounded-t-[1.2rem] bg-[#D9D9D9] h-[173px]'
-          style={{ position: 'relative', overflow: 'hidden' }}
-        >
-          <Image
-            src={
-              competition.competition_image_path
-                ? `${dbUrl}/competitions/${competition.id}/image`
-                : '/comp.jpg'
+      <div style={{cursor:'pointer'}} className="flex flex-none flex-col h-[400px] w-[300px] rounded-[1.2rem] bg-gradient-to-br from-[#4E35BE] to-[#241958] transform transition-transform duration-300 hover:scale-105">
+      <div
+        className='flex flex-none items-center justify-center rounded-t-[1.2rem] bg-[#D9D9D9] h-[173px]'
+        style={{ position: 'relative', overflow: 'hidden' }}
+      >
+        <Image
+          src={
+            competition.competition_image_path
+              ? `http://localhost:8080/competitions/${competition.id}/image`
+              : '/comp.jpg'
+          }
+          alt='Competition image'
+          className='fit'
+          fill={true}
+          sizes='50vw'
+          style={{
+            objectFit: 'cover'
             }
-            alt='Competition image'
-            className='fit'
-            fill={true}
-            sizes='50vw'
-            style={{
-              objectFit: 'cover'
-            }
-            }
-          />
-        </div>
-        <div className='item-container flex flex-col text-[16px] font-outfit p-4' style={{ height: '100%' }}>
-          <span className="flex items-center text-[24px] truncate font-odibee"
-            style={{
-              lineHeight: '1.2',
-              textAlign: 'start',
-              display: 'inline-block',
-              maxWidth: '100%',
-              color: 'white',
-            }}>
-            {competition.competition_name}
-          </span>
-          <span className='flex items-center' style={{ color: 'white' }}>
+          }
+        />
+      </div>
+      <div className='item-container flex flex-col text-[16px] font-outfit p-4' style={{ height:'100%'}}>
+      <span className="flex items-center text-[24px] truncate font-odibee" 
+       style={{
+        lineHeight: '1.2',
+        textAlign: 'start',
+        display: 'inline-block', 
+        maxWidth: '100%',
+        color: 'white',
+      }}>
+  {competition.competition_name}
+</span>
+          <span className='flex items-center' style={{color:'white'}}>
             <Image
               src='/controller.svg'
               alt='controller'
@@ -82,7 +80,7 @@ export default function CompetitionSearchItem(competition: CompetitionData) {
             />
             {gameName || 'Loading...'}
           </span>
-          <span className='flex items-center' style={{ color: 'white' }}>
+          <span className='flex items-center' style={{color:'white'}}>
             <Image
               src='/customer.svg'
               alt='customer'
@@ -92,7 +90,7 @@ export default function CompetitionSearchItem(competition: CompetitionData) {
             />
             {competition.participants} participants
           </span>
-          <span className='flex items-center' style={{ color: 'white' }}>
+          <span className='flex items-center' style={{color:'white'}}>
             <Image
               src='/calendar.svg'
               alt='calendar'
@@ -102,8 +100,8 @@ export default function CompetitionSearchItem(competition: CompetitionData) {
             />
             Begins {formatDate(competition.competition_start_date)}
           </span>
-          <span className='flex items-center' style={{ color: 'white' }}>
-            <Image
+          <span className='flex items-center' style={{color:'white'}}>
+          <Image
               src={competition.competition_type === 0 ? '/classic_mode_icon.svg' : (competition.competition_type === 1 ? '/tournament.svg' : '/rival_mode_icon.svg')}
               alt='mode'
               className='h-10 w-10 p-1.5'
@@ -113,8 +111,8 @@ export default function CompetitionSearchItem(competition: CompetitionData) {
             />
             {competitionTypeEnum[competition.competition_type]}
           </span>
-        </div>
       </div>
-    </Link>
+    </div>
+     </Link>
   )
 }
